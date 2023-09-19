03433cdccc330911f757a2d8813657f5c3691510
Fatou <ndeyefatousene1502@gmail.com>
2023-09-19 20:18:51 +0000

    loading and processing data

--- src/process.py
+++ src/process.py
@@ -1,4 +1,16 @@
-"""Contient le preprocessing des données."""
+"""Contient le preprocessing des donnï¿½es."""
+from utils import load_data
+from utils import preprocessing_duplicates
+from utils import preprocessing_null
+from utils import preprocessing_outliers
 
-def processing():
-    pass
+data = load_data('data/raw/Crop_recommendation.csv')
+print (data.head())
+
+# Preprocessing data (duplicates, outliers, null )
+data = preprocessing_duplicates(data)
+data = preprocessing_outliers(data)
+data = preprocessing_null(data)
+
+data.to_csv("data/processed/processed_data.csv", index = False)
+data.to_csv("data/cleaned/cleaned_data.csv", index = False)
