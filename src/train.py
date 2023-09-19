52bed6c114422b81d44d1a1a066b41b1b9830337
Fatou <ndeyefatousene1502@gmail.com>
2023-09-19 22:06:36 +0000

    training the model

--- src/train.py
+++ src/train.py
@@ -1,4 +1,31 @@
- """Contient entrainement du modele."""
+import pandas as pd
+from utils import load_data
+from utils import split_dataset
+from utils import encoding
+from utils import normalize
+from utils import training
+from utils import evaluate_model
 
-def training():
-    pass
+# Loading cleaned data
+data = load_data("C:/Users/USER/Documents/Master2 DIT/Outil versioning/branche1/arborescence_tree/data/cleaned/cleaned_data.csv")
+
+# Splitting data
+y = data['label']
+X = data.drop('label', axis=1)
+X_train, X_test, y_train, y_test = split_dataset(X, y)
+
+# y Label encoding
+y_train = encoding(y_train)
+y_test = encoding(y_test)
+
+# Normalize X features
+X_train = normalize(X_train)
+X_test = normalize(X_test)
+
+# Training
+y_pred = training(X_train, y_train, X_test)
+
+# Evaluate model
+accuracy, report = evaluate_model(y_test, y_pred)
+print(f"The accuracy is {accuracy}")
+print(f"The report is \n {report}")
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,11 @@ This module contains methods to preprocess the data
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 def load_data(filepath):
     # loading dataset
@@ -41,7 +46,33 @@ def preprocessing_outliers(data_df):
     return data_df
 
 
+def split_dataset(X, y):
+    # This method helps to split the data to train and test datasets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+    return X_train, X_test, y_train, y_test 
 
-    
+# label encoder
+def encoding(label):
+    le= LabelEncoder()
+    label =le.fit_transform(label)
+    return label
+
+# Normalization/Standardisation
+def normalize(features):
+    features = StandardScaler().fit_transform(features)
+    return features
+
+# Training
+def training(X_train, y_train, X_test):    
+    rfc = RandomForestClassifier(n_estimators=100, random_state=24)
+    rfc.fit(X_train, y_train)
+    y_pred = rfc.predict(X_test)
+    return y_pred
+
+# Evaluate model
+def evaluate_model(y_test, y_pred):
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+    return accuracy, report
 
 
